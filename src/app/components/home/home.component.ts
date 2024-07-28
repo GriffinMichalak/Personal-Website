7970a8ede4ef67ffed49e8431e7a4bd9a6ddd850
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2024-07-27 21:06:16 -0400

    add contact info

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss', '/src/styles.scss']
 })
 export class HomeComponent {
   confetti = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
