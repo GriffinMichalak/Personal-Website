7a47a16b3746b79bafcd291af8fdd1bcf6ecaa8f
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2024-08-03 14:23:54 -0400

    added portfolio component

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from '../../models/project'; 
 
 @Component({
   selector: 'app-portfolio',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+
+  projects: Project[] = [
+    new Project("FindDairyFree", "assets/png/finddairyfree.jpeg", "https://github.com/GriffinMichalak/FindDairyFree"),
+    new Project("Connections Clone", "assets/png/connections.png", "https://nyt-connections-nine.vercel.app/"),
+    new Project("Wordle Clone", "assets/png/wordle.png", "https://wwordle.vercel.app/"),
+    new Project("Snake Game", "assets/png/snake.png", "https://snak3.vercel.app/"),
+    new Project("This Website", "assets/png/personalwebsite.png", "https://griffinmichalak.vercel.app"),
+  ]
 
 }
